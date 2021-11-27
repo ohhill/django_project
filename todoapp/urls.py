f54a3ab74c5ef7e832d5df90ad7da2c9e4549cf8
AlexGillll <sashahylyuk@gmail.com>
2021-11-27 18:28:41 +0200

    add path admin in urls todoapp

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 app_name = 'todoapp'
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path('admin', admin.site.urls),
 ]
