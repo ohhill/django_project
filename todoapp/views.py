d336dc25b49b4fcbc085e2c14a5b99cb03b138e5
AlexGillll <sashahylyuk@gmail.com>
2021-11-27 15:13:20 +0200

    add a context views and add for in html

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+task = ['foo', 'bar', 'baz']
+
 def index(request):
-    return render(request, "todoapp/index.html")
+    return render(request, "todoapp/index.html", {
+        'tasks' : task
+    })
