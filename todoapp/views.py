f95d1136b670a8d578ea666cff312766e9caecb2
AlexGillll <sashahylyuk@gmail.com>
2021-11-27 15:21:53 +0200

    create form

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django import forms
+from django.urls import reverse
 
 
 task = ['foo', 'bar', 'baz']
 
+class NewTextForm(forms.Form):
+    tasks = forms.CharField(label="New Task:")
+
 def index(request):
     return render(request, "todoapp/index.html", {
-        'tasks' : task
+        'tasks' : task,
+        'form' : NewTextForm(),
     })
